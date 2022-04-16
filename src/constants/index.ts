f4d71a0a89eab0ed6babe7804a90a513a96969b0
Lohsea <915374238@qq.com>
2022-04-16 20:33:34 +0800

    feat(guide): add guide

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: lixin
  * @Date: 2021-08-27 11:52:05
- * @LastEditTime: 2022-04-13 17:10:59
+ * @LastEditTime: 2022-04-16 17:36:42
  *
  */
 
@@ -17,8 +17,9 @@ export const WSSURL = "wss://peregrine.kilt.io/parachain-public-ws/";
 // 'BOTLabs Trusted Entity': 'wss://spiritnet.kilt.io',
 // export const HOSTPREFIX = 'http://107.191.53.163:3009'
 // export const HOSTPREFIX = "https://api.credential.zcloak.network";
-export const HOSTPREFIX = "http://api.dev-credential.zcloak.network";
-// export const HOSTPREFIX = "http://192.168.31.198:7001";
+// export const HOSTPREFIX = "http://api.dev-credential.zcloak.network";
+export const HOSTPREFIX = "http://192.168.31.198:7001";
+export const PROOFHOSTPREFIX = "http://192.168.31.198:7002";
 
 // export const HOSTPREFIX = "http://45.77.37.78:7001";
 // 日期格式配置
