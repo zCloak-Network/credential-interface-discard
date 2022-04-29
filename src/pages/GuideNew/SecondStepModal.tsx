50444e3a82122736b0ccd7e0c70b9b623c31d899
Lohsea <915374238@qq.com>
2022-04-29 17:41:56 +0800

    fix(guide): update guide description

--- src/pages/GuideNew/SecondStepModal.tsx
+++ src/pages/GuideNew/SecondStepModal.tsx
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: lixin
  * @Date: 2022-04-11 23:15:44
- * @LastEditTime: 2022-04-18 16:25:21
+ * @LastEditTime: 2022-04-29 17:39:09
  */
 import React, { useMemo, useState } from "react";
 import Modal from "../../components//Modal";
@@ -47,23 +47,26 @@ const SecondStepModal: React.FC = () => {
       <div className="email">
         <p className="email-title">Dear Adventurer, </p>
         <p>
-          In the zCloak Kingdom, you must have a zkPortrait to identify
-          yourself.
+          Welcome to the zCloak Kingdom! Here you can team up with people to
+          explore a dungeon. Of course, it would be easier if you know each
+          other in advance—such as your experience, damage type or equipment
+          level.
         </p>
         <p>
-          zCloak Kingdom will offer three pieces of random rarity equipment for
-          each new adventurer.
+          In zCloak Kingdom, we treat your privacy as the first priority. Thus
+          we have introduced a POAP system to represent you without giving away
+          your detailed information. Here is how it works.
         </p>
         <p>
-          Depending on your profession and age, you should have different
-          portraits and the average of all equipment rarities will determine
-          your equipment level.
+          First, you need to fill in some basic information. Our secrecy agency
+          will check it, attest to it and issue an identity credential to you.
         </p>
         <p>
-          In this example, we demonstrate a way to guide you to create your
-          zkPortrait without revealing your specific profession, age, or
-          equipment rarity.
+          Second, use the zCloak ID Wallet to generate a STARK proof based on
+          your credential. Our scholars will verify the validity of your proof.
+          If the proof is correct, a POAP will be offered to you.
         </p>
+        <p>Use it with your teammates and have an awesome journey!</p>
         <p className="email-footer">zCloak Kingdom</p>
       </div>
       <div className="btn-wrapper">
